c9217b2efe15bdf47b998a88522d1bd18db8430c
rajashekarcs2023 <vrajashekar2022mscs@gmail.com>
2024-08-12 11:09:07 -0700

    1.2 setting up

---
+++ src/main/java/com/company/IntroductionToSpringBoot/Apple.java
@@ -0,0 +1,9 @@
+package com.company.IntroductionToSpringBoot;
+
+public class Apple {
+
+    void eatApple(){
+        System.out.println("I am eating the apple");
+
+    }
+}
---
+++ src/main/java/com/company/IntroductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -0,0 +1,21 @@
+package com.company.IntroductionToSpringBoot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBootApplication implements CommandLineRunner {
+	Apple obj;
+	public static void main(String[] args) {
+
+		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		obj.eatApple();
+	}
+}
